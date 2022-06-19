e33488d8a4c5019f58417be1ee9e3eaf187fe932
CarlosGuerrero9909 <caraguerrerob@correo.udistrital.edu.co>
2022-06-18 21:30:52 -0500

    primer endpoint

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,request
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<p>Hello, World!<p>'
+@app.route("/", methods = ["GET", "POST"])
+def hello():
+    if request.method == "GET":
+        numero = random.randint(1, 10000)
+        return "Hola mundo: {}".format(numero)
+    if request.method == "POST":
+        nombre = request.form['nombre']
+        return "Hola {}".format(nombre)
 
-@app.route('/goodbye')
-def hello_world():
-    return '<p>Good Bye, World!<p>'
+if __name__ == "__main__":
+    app.run(debug=True)
